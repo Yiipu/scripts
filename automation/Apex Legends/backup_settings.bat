4109898070f37bb509cab1be1fa3bc8b19e8bd82
Opal Kim <opal.kim@icloud.com>
2025-06-07 02:23:34 +0800

    Add files via upload

---
+++ automation/Apex Legends/backup_settings.bat
@@ -0,0 +1,1 @@
+copy /Y "%USERPROFILE%\Saved Games\Respawn\Apex\local\settings.cfg" ".\settings.cfg"
---
+++ automation/Apex Legends/override_settings.bat
@@ -0,0 +1,1 @@
+copy /Y ".\settings.cfg" "%USERPROFILE%\Saved Games\Respawn\Apex\local\settings.cfg"
